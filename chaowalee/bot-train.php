1e7f55974ca79c8226ebdb01ea9d182873363a4b
tueseed <tue_seed@hotmail.com>
2018-09-21 22:22:34 +0700

    Update bot-train.php

--- chaowalee/bot-train.php
+++ chaowalee/bot-train.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
             $first_char = substr($txtin,0,1);//ตัดเอาเฉพาะตัวอักษรตัวแรก
 			if($txtin == "liff")
 			{
-				$txtsend = "line://app/1565368462-832ar4g6";
+				$txtsend = "line://app/1565368462-KLLJ0nRV";
 				reply_msg($txtsend,$replyToken);//เรียกใช้ function
 				break;
 			}
