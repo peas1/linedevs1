3cdf04ab6ecf80ec8a694df2f7cea09106548e1e
tueseed <tue_seed@hotmail.com>
2018-09-26 10:26:34 +0700

    Add files via upload

---
+++ nutt.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>เว็บไซต์ทดสอบ</title>
+    </head>
+    <body>
+        <h1>ทดสอบการแสดงผล</h1>
+        <input type="text" class="form-control" id="usr">
+    </body>
+</html>
