637cabee27bf78c547d59192b510e1aec7f61cff
tueseed <tue_seed@hotmail.com>
2018-10-05 15:29:28 +0700

    Update connect-db-eass1.php

--- db/connect-db-eass1.php
+++ db/connect-db-eass1.php
@@ -1,8 +1,8 @@
  <?php
  	$server = "us-cdbr-iron-east-01.cleardb.net";
-    $username = "bde5ae12f51085";
-    $password = "3c146dba";
-    $db = "heroku_4535866affeb67f";
+    $username = "b798786b8aa714";
+    $password = "2e0e0451";
+    $db = "heroku_ce52199dd4f50e1";
     $conn = new mysqli($server, $username, $password, $db);
     // Check connection
     if ($conn->connect_error) 
