e6760059e218a115af3b1fe7398d775f6046c057
tueseed <tue_seed@hotmail.com>
2018-10-05 18:56:14 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "THIS IS  THE TEAM OF PEA S1......";
-?>
+<html>
+    <head>
+        <title>เว็บไซต์ทดสอบ</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <h1>EASS1</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <input type="button" class="form-control" id="text1" name="text1" onclick="window.location.href'https://eass1-bot.herokuapp.com/nutthapong/'">
+                </div>
+            </div>
+        </div> 
+    </body>
+</html>
