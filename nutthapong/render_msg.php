f162958d688d715c8170b1ebc33b3a74e8080732
tueseed <tue_seed@hotmail.com>
2018-10-07 09:27:34 +0700

    Add files via upload

--- nutthapong/render_msg.php
+++ nutthapong/render_msg.php
@@ -35,8 +35,48 @@ function flex_msg()
 									"flex": 0
 									}
 			}';	
-	$result = json_decode($json);
-	return $json;
+	$json1 = '{
+				"type":"flex",
+				"altText":"ทดสอบ Flex Message",
+				"contents":[{
+								"type": "bubble",
+								"body": {
+											"type": "box",
+											"layout": "vertical",
+											"contents": [{
+															"type": "text",
+															"text": "รายงานสถานะงานก่อสร้าง",
+															"weight": "bold",
+															"size": "xl"
+														}]
+										},
+								"footer": {
+											"type": "box",
+											"layout": "vertical",
+											"spacing": "sm",
+											"contents": [{
+													"type": "button",
+													"style": "link",
+													"height": "sm",
+													"action": {
+																"type": "uri",
+																"label": "รายละเอียดเพิ่มเติม",
+																"uri": "https://linecorp.com"
+															}
+												},
+												{
+													"type": "spacer",
+													"size": "sm"
+												}
+												],
+									"flex": 0
+									}
+					
+				}]
+		
+	}';
+	$result = json_decode($json1);
+	return $result;
 }
 
 
