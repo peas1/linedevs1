8add4d883d86b02a6c3b34a5ee45715d8b1f7d6b
tueseed <tue_seed@hotmail.com>
2018-10-09 21:30:54 +0700

    Add files via upload

--- nutthapong/insert_data.php
+++ nutthapong/insert_data.php
@@ -8,14 +8,20 @@
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+		<style type="text/css">
+			.row-center
+			{
+				text-align:center;
+			}
+		</style>
 	</head>
 	<body>
 		<div class="container-fluid" style="background-color:yellow;">
-			
-			<div class="col-lg-4 offset-lg-4" style="background-color:red;">
-				<h4>บันทึกข้อมูล</h4>
+			<div class="row row-center">
+				<div class="col-lg-4 offset-lg-4" style="background-color:red;">
+					<h4>บันทึกข้อมูล</h4>
+				</div>
 			</div>
 		</div>
-		
 	</body>
 </html>
