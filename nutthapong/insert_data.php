098525832482fc3e522076478495d1fb5b31d646
tueseed <tue_seed@hotmail.com>
2018-10-09 22:25:06 +0700

    Add files via upload

--- nutthapong/insert_data.php
+++ nutthapong/insert_data.php
@@ -24,37 +24,39 @@
 			</div>
 		</div>
 		<div class="container-fluid">
-			<div class="col-lg-2">
-				<div class="row">
-					<label for="name">ชื่อ :</label>
-					<input class="form-control" type="text" name="name" id="name" placeholder="ใส่ชื่อของคุณ">
+			<div class="row">
+				<div class="col-lg-2">
+					<div class="row">
+						<label for="name">ชื่อ :</label>
+						<input class="form-control" type="text" name="name" id="name" placeholder="ใส่ชื่อของคุณ">
+					</div>
+					<div class="row">
+						<label for="lastname">นามสกุล :</label>
+						<input class="form-control" type="text" name="lastname" id="lastname" placeholder="ใส่นามสกุลของคุณ">
+					</div>
+					<div class="row">
+						<label for="email">ชื่อ :</label>
+						<input class="form-control" type="text" name="email" id="email" placeholder="ใส่ email ของคุณ">
+					</div>
+					<div class="row">
+						<label for="email">ชื่อ :</label>
+						<input class="btn btn-success btn-block" type="submit">
+					</div>
 				</div>
-				<div class="row">
-					<label for="lastname">นามสกุล :</label>
-					<input class="form-control" type="text" name="lastname" id="lastname" placeholder="ใส่นามสกุลของคุณ">
-				</div>
-				<div class="row">
-					<label for="email">ชื่อ :</label>
-					<input class="form-control" type="text" name="email" id="email" placeholder="ใส่ email ของคุณ">
-				</div>
-				<div class="row">
-					<label for="email">ชื่อ :</label>
-					<input class="btn btn-success btn-block" type="submit">
-				</div>
-			</div>
-			<div class="col-lg-10">
-				<div class="table-responsive">
-					<table class="table">
-						<thead>
-							<tr>
-								<th>ชื่อ</th>
-								<th>นามสกุล</th>
-								<th>email</th>
-							</tr>
-						</thead>
-						<tbody>
-						</tbody>
-					</table>
+				<div class="col-lg-10">
+					<div class="table-responsive">
+						<table class="table">
+							<thead>
+								<tr>
+									<th>ชื่อ</th>
+									<th>นามสกุล</th>
+									<th>email</th>
+								</tr>
+							</thead>
+							<tbody>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
