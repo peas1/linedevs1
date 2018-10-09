a27df002f7bec308a5280f416e4b7be74e56c306
tueseed <tue_seed@hotmail.com>
2018-10-09 14:01:31 +0700

    Add files via upload

---
+++ nutthapong/mysql1.php
@@ -0,0 +1,14 @@
+ <?php
+ 	$server = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "b798786b8aa714";
+    $password = "2e0e0451";
+    $db = "heroku_ce52199dd4f50e1";
+    $conn = new mysqli($server, $username, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_query($conn, "SET NAMES utf8");
+	echo "connected !!!!!!!";
+	
+?>
