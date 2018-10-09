657167a21a3a60406d5a385808ecc8fae1f84db8
tueseed <tue_seed@hotmail.com>
2018-10-09 21:38:30 +0700

    Add files via upload

--- nutthapong/insert_data.php
+++ nutthapong/insert_data.php
@@ -25,7 +25,7 @@
 		</div>
 		<div class="mt-5 container-fluid" style="background-color:blue;">
 			<div class="row row-center">
-				<div class="col-lg-4 offset-lg-4" style="background-color:red;">
+				<div class="mt-5 col-lg-4 offset-lg-4" style="background-color:red;">
 					<h4>บันทึกข้อมูล</h4>
 				</div>
 			</div>
