a3ee4a0ad88be9d90a2b1c7e91e9964ea79d73b2
tueseed <tue_seed@hotmail.com>
2018-10-10 11:24:21 +0700

    Add files via upload

--- nutthapong/recive.php
+++ nutthapong/recive.php
@@ -1,4 +1,26 @@
 <?php
+$method = $_SERVER["REQUEST_METHOD"];
+if($method == "POST")
+{
+	$server = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "b798786b8aa714";
+    $password = "2e0e0451";
+    $db = "heroku_ce52199dd4f50e1";
+    $conn = new mysqli($server, $username, $password, $db);
+	mysqli_query($conn, "SET NAMES utf8");
+	
 	$name = $_POST["name"];
-	echo $name;
+	$lastname = $_POST["lastname"];
+	$email = $_POST["email"];
+	//echo $name." ".$lastname." ".$email;
+	$sql = "INSERT INTO tbl_nuthapong(name,lastname,email) VALUES('$name','$lastname','$email')";
+	$result = mysqli_query($conn, $sql) or trigger_error($conn->error."[$sql_insert]");
+	echo '<script type="text/javascript">';
+	echo 'window.location.href="insert_data.php";';
+	echo '</script>';
+}
+else
+{
+	echo "Method Not Allow!!!!";
+}
 ?>
