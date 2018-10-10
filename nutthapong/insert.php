1de94976c363b368fe93be5ca3b536225119efa2
tueseed <tue_seed@hotmail.com>
2018-10-10 10:10:52 +0700

    Add files via upload

--- nutthapong/insert.php
+++ nutthapong/insert.php
@@ -27,7 +27,21 @@
 		<div class="container"> 
 			<div class="row">
 				<div class="col-lg-2" style="background-color:yellow;">
-					<h4>col1</h4>
+					<div class="row">
+						<label for="usr">ชื่อ :</label>
+						<input type="text" class="form-control" name="name" placeholder="ใส่ชื่อของคุณ">
+					</div>
+					<div class="row">
+						<label for="usr">นามสกุล :</label>
+						<input type="text" class="form-control" name="lastname" placeholder="ใส่นามสกุลของคุณ">
+					</div>
+					<div class="row">
+						<label for="usr">email :</label>
+						<input type="text" class="form-control" name="email" placeholder="ใส่ email ของคุณ">
+					</div>
+					<div class="row">
+						<input class="btn btn-success btn-block" type="submit" value="บันทึก">
+					</div>
 				</div>
 				<div class="col-lg-10" style="background-color:blue;">
 					<h4>col2</h4>
