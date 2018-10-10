2cecbc54f4de20a350237b92d6d291283919ed54
tueseed <tue_seed@hotmail.com>
2018-10-10 11:26:19 +0700

    Add files via upload

--- nutthapong/recive.php
+++ nutthapong/recive.php
@@ -16,7 +16,7 @@ if($method == "POST")
 	$sql = "INSERT INTO tbl_nuthapong(name,lastname,email) VALUES('$name','$lastname','$email')";
 	$result = mysqli_query($conn, $sql) or trigger_error($conn->error."[$sql_insert]");
 	echo '<script type="text/javascript">';
-	echo 'window.location.href="insert_data.php";';
+	echo 'window.location.href="insert.php";';
 	echo '</script>';
 }
 else
