e8dd8609fe71ca23b65012f07fbaa931fc941525
tueseed <tue_seed@hotmail.com>
2018-10-12 13:21:00 +0700

    Update test.php

--- kritsada/test.php
+++ kritsada/test.php
@@ -10,7 +10,7 @@
   $sql_area = "SELECT * FROM tbl_improve WHERE detail LIKE '%".$office_id."%'";
 	$query_area = mysqli_query($conn,$sql_area);
   $num_row = mysqli_num_rows($query_area);
-  echo "ค้นพบ ".$num_row." รายการ."<br>";
+  echo "ค้นพบ ".$num_row." รายการ<br>";
   while($obj = mysqli_fetch_array($query_area))
   {
     echo $obj["pea"]."  ".$obj["pea"]."  ".$obj["detail"]."  ".$obj["wbs"]."<br>";
