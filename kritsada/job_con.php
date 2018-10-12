a985cc399bfd6ceccfa586fd9b3e60de66eaf582
tueseed <tue_seed@hotmail.com>
2018-10-12 14:51:45 +0700

    Add files via upload

---
+++ kritsada/job_con.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<title>ข้อมูลงานก่อสร้าง</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0," charset="utf-8">
+		<title>BOT TRAIN PEA S.1</title>
+		<link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+		<style type="text/css">
+			.row-center
+			{
+				text-align:center;
+			}
+			container-fluid 
+			{
+				font-family: 'Kanit', sans-serif;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container-fluid" style="background-color:#4285F4;">
+			<div class="row row-center">
+				<div class="col-sm-12">
+					<h4>ข้อมูลงานก่อสร้าง</h4>
+				</div>
+			<div>
+		<div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-10 offset-sm-1">
+					<div class="row">
+						<label for="usr">คำค้นหา :</label>
+						<input type="text" class="form-control" name="name" placeholder="การไฟฟ้า หรือ ชื่องาน">
+					</div>
+				</div>
+			<div>
+			<div class="mt-2 row">
+				<div class="col-sm-10 offset-sm-1">
+					<div class="row">
+						<input class="btn btn-primary btn-block" type="submit" value="ค้นหา">
+					</div>
+				</div>
+			<div>
+		<div>
+	</body>
+</html>
