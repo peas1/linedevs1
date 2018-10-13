c0d4e92846a29122b40c2a190b1720642a5f8008
tueseed <tue_seed@hotmail.com>
2018-10-13 13:05:23 +0700

    Add files via upload

--- nutthapong/liff-starter.js
+++ nutthapong/liff-starter.js
@@ -1,16 +1,9 @@
 window.onload = function (e) 
 				{
-					liff.init(
-						  data => {
-							// Now you can call LIFF API
-							const userId = data.context.userId;
-						  },
-						  err => {
-							// LIFF initialization failed
-						  }
-						);
+
 					liff.getProfile().then(profile => {
-									  document.getElementById('lid').value = profile.userId
+									   var LID = document.getElementById('lid').value = profile.userId;
+									   alert("amv,kal;nl");
 									})
 									.catch((err) => {
 									  console.log('error', err);
