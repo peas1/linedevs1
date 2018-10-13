01d83ae683cee0efdcb63fa7b7b7a5551942d52e
tueseed <tue_seed@hotmail.com>
2018-10-13 13:03:01 +0700

    Add files via upload

--- nutthapong/liff-starter.js
+++ nutthapong/liff-starter.js
@@ -1,7 +1,16 @@
 window.onload = function (e) 
 				{
+					liff.init(
+						  data => {
+							// Now you can call LIFF API
+							const userId = data.context.userId;
+						  },
+						  err => {
+							// LIFF initialization failed
+						  }
+						);
 					liff.getProfile().then(profile => {
-									  document.getElementById('lid').value = profile.displayName
+									  document.getElementById('lid').value = profile.userId
 									})
 									.catch((err) => {
 									  console.log('error', err);
