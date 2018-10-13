e7196beb3cfc7401812826386369c552173c6428
tueseed <tue_seed@hotmail.com>
2018-10-13 13:13:15 +0700

    Add files via upload

--- nutthapong/liff-starter.js
+++ nutthapong/liff-starter.js
@@ -1,12 +1,8 @@
 window.onload = function (e) 
 				{
 					liff.init(function (data) {});
-					liff.getProfile().then(profile => {
-									   var LID = document.getElementById('lid').value = profile.userId;
-									  
-									})
-									.catch((err) => {
-									  console.log('error', err);
-									});
+					liff.getProfile().then(function (profile) {
+						document.getElementById('displayname').value = profile.displayName;
+					});
 				};
 				
