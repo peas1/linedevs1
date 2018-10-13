0372b3b01e4a9bbb8c87c38fa410f2d6d975f36c
tueseed <tue_seed@hotmail.com>
2018-10-13 13:23:29 +0700

    Add files via upload

--- nutthapong/liff-starter.js
+++ nutthapong/liff-starter.js
@@ -3,6 +3,14 @@ window.onload = function (e)
 					liff.init(function (data) {});
 					liff.getProfile().then(function (profile) {
 						document.getElementById('displayname').value = profile.displayName;
-					});
+					    liff.sendMessages([
+											{
+											type: 'text',
+											text: 'From:' + profile.displayName
+											}
+										]);
+					}
+					);
+					
 				};
 				
