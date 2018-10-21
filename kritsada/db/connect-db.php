3a0d727bc70da7291dbd3ef61735317395b94bd1
kkm005 <33860679+kkm005@users.noreply.github.com>
2018-10-21 21:46:51 +0700

    Delete connect-db.php

--- kritsada/db/connect-db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$server = "us-cdbr-iron-east-01.cleardb.net";
-	$username = "bb638a0b9e5724";
-	$password = "3556cc19";
-	$db = "heroku_5663ecc9ac15f3e";
-	$conn = new mysqli($server, $username, $password, $db);
-	mysqli_query($conn, "SET NAMES UTF8");
-?>
