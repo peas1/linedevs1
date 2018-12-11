f419a386b02ae28d6015355aaed1a6ba93e60830
tueseed <tue_seed@hotmail.com>
2018-12-11 23:16:03 +0700

    Add files via upload

--- warote/upload-xls.php
+++ warote/upload-xls.php
@@ -2,11 +2,11 @@
     require('connect-db.php');
     require('manage_xls.php');
     
-    //$target_path = uploadXLSXFile($conn, $_FILES['vocfile']);//<< Function นี้อยู่ใน utils.php
+    $target_path = uploadXLSXFile($conn, $_FILES['vocfile']);//<< Function นี้อยู่ใน utils.php
     //$namedDataArray = getDataFromXLSXPath($target_path);//<< Function นี้อยู่ใน utils.php
     //clearComplaintData($conn);//<< Function นี้อยู่ใน utils.php
     //insertComplaintData($conn, $namedDataArray);//<< Function นี้อยู่ใน utils.php
     //$number_complaint = countComplaintData($conn);
-    echo "ได้เพิ่มข้อมูลแล้วทั้งหมด ";
+    echo "ได้เพิ่มข้อมูลแล้วทั้งหมด ".$_FILES['vocfile'];
 
  ?>
