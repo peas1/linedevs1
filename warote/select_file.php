e8332b8ab24a4a20d8763247f9eb3984986f2f51
tueseed <tue_seed@hotmail.com>
2018-12-11 22:16:33 +0700

    Add files via upload

--- warote/select_file.php
+++ warote/select_file.php
@@ -9,7 +9,6 @@
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
-		<script src="upload.js"></script>
 		<style type="text/css">
 			.row-center
 			{
@@ -32,13 +31,15 @@
 		<div class="mt-2 container"> 
 			<div class="row">
 				<div class="col-lg-2" >
+					<form name="voc-form" id="voc-form" method="POST" enctype="multipart/form-data">
 						<div class="row">
 							<label for="vocfile">ไฟล์ xls</label>
 							<input type="file" required name="vocfile" class="form-control-file btn btn-dark" id="vocfile" accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
 						</div>
 						<div class="mt-2 row">
-							<input class="btn btn-success btn-block" type="submit" value="Upload" onclick="upload()">
+							<input class="btn btn-success btn-block" type="submit" value="Upload" >
 						</div>
+					</form>
 				</div>
 				<div class="col-lg-10">
 					<div class="table-responsive">
@@ -75,4 +76,36 @@
 			</div>
 		</div>
 	</body>
+	<script>
+        $(function(){
+            $('[id="voc-form"]').submit(function(event){
+                event.preventDefault();
+                var formData = new FormData($(this)[0]);
+                $.ajax({
+                    url: 'upload-xls.php',
+                    method: 'POST',
+                    data: formData,
+                    async: true,
+                    cache: false,
+                    contentType: false,
+                    enctype: 'multipart/form-data',
+                    processData: false,
+                    beforeSend: function(){
+                        $.blockUI({ message:'<h3>Uploading xlsx file...</h3>' });
+                    },
+                    success: function(response) {
+                        alert(response);
+                    },
+                    error: function(response){
+                        console.log('[error]', response);
+                    },
+                    complete: function() {
+                        $.unblockUI();
+                        location.reload();
+                    }
+                });
+                return false;
+            });
+        });
+</script>
 </html>
