ebbd3c68532b688b29c1eaa1512f7c5b90a5401b
tueseed <tue_seed@hotmail.com>
2018-12-11 09:39:34 +0700

    Add files via upload

---
+++ warote/connect-db.php
@@ -0,0 +1,12 @@
+ <?php
+ 	$server = "us-cdbr-iron-east-01.cleardb.net";
+	$username = "b798786b8aa714";
+	$password = "2e0e0451";
+	$db = "heroku_ce52199dd4f50e1";
+	$conn = new mysqli($server, $username, $password, $db);
+	mysqli_query($conn, "SET NAMES utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_query($conn, "SET NAMES utf8");
+?>
