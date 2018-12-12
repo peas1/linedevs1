c0261967080a1eb211505e67c4f4caaca9650863
tueseed <tue_seed@hotmail.com>
2018-12-12 14:33:40 +0700

    Add files via upload

--- nutthapong/test_db.php
+++ nutthapong/test_db.php
@@ -7,7 +7,7 @@
 	$conn = new mysqli($server, $username, $password, $db);
 	mysqli_query($conn, "SET NAMES utf8");
 	
-	$sql_text = "SELECT * FROM contacttable WHERE name = 'วิท";
+	$sql_text = "SELECT * FROM contacttable WHERE name = 'วิท'";
 	$query_text = mysqli_query($conn,$sql_text);
 	
 	while($obj = mysqli_fetch_array($query_text))
