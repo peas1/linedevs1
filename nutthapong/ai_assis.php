d227a40349fb00b5d6a16e0ddd47311f609d8489
tueseed <tue_seed@hotmail.com>
2018-12-20 15:51:20 +0700

    Add files via upload

--- nutthapong/ai_assis.php
+++ nutthapong/ai_assis.php
@@ -6,9 +6,17 @@ if($method == "POST")
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	//$keyword = $json->result->parameters->text;
-		
+	$res='{
+			"fulfillmentText": "This is a text response",
+			"source": "example.com",
+			"payload": {
+						"line": {
+										"text": "'.$json.'"
+									}
+						}
+		 }'	
 	header("Content-Type: application/json");
-	echo json_encode($json);	
+	echo json_encode($res);	
 }else{
 	echo "Method Not allow";
 }
