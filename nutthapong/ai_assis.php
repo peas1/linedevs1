788658194fc7b18b02ddb100acd0994fe5fb18cf
tueseed <tue_seed@hotmail.com>
2018-12-22 22:47:55 +0700

    Add files via upload

--- nutthapong/ai_assis.php
+++ nutthapong/ai_assis.php
@@ -30,12 +30,12 @@ if($method == "POST")
 	header("Content-Type: application/json");
 	$data = json_encode($json);
 	$replyToken = $json->originalRequest->data->data->replyToken;
-	$topic = $json->result->parameters->topic;
+	/*$topic = $json->result->parameters->topic;
 	$day = $json->result->parameters->day;
 	$time_period = $json->result->parameters->t_period;
 	$place = $json->result->parameters->place;
 	$Attendees = $json->result->parameters->Attendees;
-	$data_back = $topic."\n".$day."\n".$time_period."\n".$place."\n".$Attendees;
+	$data_back = $topic."\n".$day."\n".$time_period."\n".$place."\n".$Attendees;*/
 	reply_msg($data,$replyToken);
 	//push($replytoken);
 	echo json_encode($res3);
