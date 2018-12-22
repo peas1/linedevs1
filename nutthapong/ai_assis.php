a4922cfa636508629b2816775d6ec04ebc54b784
tueseed <tue_seed@hotmail.com>
2018-12-22 22:37:33 +0700

    Add files via upload

--- nutthapong/ai_assis.php
+++ nutthapong/ai_assis.php
@@ -21,57 +21,22 @@ function reply_msg($keyword,$replyToken)
     $result = curl_exec($ch);
     curl_close($ch);
     echo $result . "\r\n";
-}
-function push($data)
-{
-    $access_token = 'HScoQtJ9WeTsUePpz0xZ7vo//Tm7j+PR/LCoi09r4L7XDPJVZr/Bc3iSn6NGBJVa8LpQM446o/uIUbLxOfjm09FDX+73peOuXqHvKttcHLeqogyWj0RU/Vqj1LapFoxfp2lOPYq4O8ErqPnZGyRpPAdB04t89/1O/w1cDnyilFU=';
-    $messages = [ 'type' => 'text', 
-				'text' => $data
-                 ];
-    $url = 'https://api.line.me/v2/bot/message/push';
-    $data = ['to' => 'Ua9ba6c25071c19588c095ec147efe2b1','messages' => [$messages]];
-    $post = json_encode($data);
-    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch); 
 }	
 $method = $_SERVER["REQUEST_METHOD"];
 if($method == "POST")
 {
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-	//$keyword = $json->result->parameters->text;
-	$res='{
-					"fulfillmentText":"This is a text response",
-					"source":"line",
-					"payload":{
-								"line":{
-										"type":"text"
-										}
-							}
-			}';
-	$res2 = array(
-					"source"=>$json->result->source,
-					"payload"=>array(
-									  "line"=>array(
-													 "message"=> array(
-																		"type"=>"text",
-																		"text"=>"jkljioopjdkslajdiwd")
-													)
-									)
-				);
-	$res3 = array("message"=>array("platform"=>"line","speech"=>"Text response","type"=>0)
-				);
 	header("Content-Type: application/json");
 	$data = json_encode($json);
 	$replyToken = $json->originalRequest->data->data->replyToken;
-	reply_msg('สวัสดี',$replyToken);
+	$topic = $json->result->parameters->topic;
+	$day = $json->result->parameters->day;
+	$time_period = $json->result->parameters->t_period;
+	$place = $json->result->parameters->place;
+	$Attendees = $json->result->parameters->Attendees;
+	$data = $topic."\n".$day."\n".$time_period."\n".$place."\n".$Attendees;
+	reply_msg($data,$replyToken);
 	//push($replytoken);
 	echo json_encode($res3);
 	//echo $requestBody;
